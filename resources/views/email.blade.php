ca1629f61c5c52fe2773ddf806edfc8557afb945
YISECA <yiseca@gmail.com>
2016-12-09 11:38:55 -0500

    Formulario de inscripción

---
+++ resources/views/email.blade.php
@@ -0,0 +1,13 @@
+<html>
+
+
+
+<h1>Caminata Páramo del Verdillo 11 de Diciembre de 2016</h1>
+
+
+
+<h1>Su registro fue exitoso!</h1>
+
+
+
+<p></p>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,300 @@
+@extends('master')                              
+
+@section('content')         
+
+<link rel="stylesheet" type="text/css" href="public/Css/form.css">
+
+<link rel="stylesheet" type="text/css" href="public/Css/slider_styles.css">
+
+<section id="page1">
+
+    <div class="panel panel-default">
+
+        <div class="panel-heading">Inicio</div>
+
+        <div class="panel-body">
+
+            <div class="freebirdFormviewerViewFormContent ">
+
+                <div class="freebirdFormviewerViewHeaderHeader">
+
+                    <div class="freebirdFormviewerViewHeaderTitleRow">
+
+                        <div class="freebirdFormviewerViewHeaderTitle" dir="auto" role="heading" aria-level="1">Caminata Páramo del Verdillo 11 de Diciembre de 2016</div>
+
+                    </div>
+<br>
+                    <div class="stage">
+
+                        <div id="SLDR-ONE" class="sldr">
+
+                            <ul class="wrp animate">
+
+                                <li class="elmnt-one">                                 
+
+                                        <div class="wrap"><img src="public/Img/verdillo.jpg" width="800" height="563">
+
+                                        </div>                                   
+                                </li>
+
+                                <li class="elmnt-two">
+                                    
+                                        <div class="wrap"><img src="public/Img/verdillo1.jpg" width="800" height="563">
+
+                                        </div>                                 
+                                </li>
+
+                                  <li class="elmnt-three">
+                                    
+                                        <div class="wrap"><img src="public/Img/verdillo3.jpg" width="800" height="563">
+
+                                        </div>                                 
+                                </li>
+
+                            </ul>
+
+                        </div>
+
+                        <div class="clear"></div>
+
+                        <ul class="selectors">
+
+                            <li class="focalPoint"><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>
+                         
+                        </ul>
+
+                    </div>
+
+                    <script src="public/Js/jquery.sldr.js"></script>
+
+                    <div class="freebirdFormviewerViewHeaderDescription" dir="auto">
+
+                        <h2>Ubicación</h2>
+
+                        <p><font size="3">El sendero se encuentra ubicada en la localidad 5 de Usme, entre los barrios Tihuaque y las Violetas, limita al norte con las localidades de San Cristóbal, Rafael Uribe y Tunjuelito; al sur con la localidad de Sumapaz, al oriente con los municipios de Ubaque y Chipaque y al occidente con la localidad de Ciudad Bolívar y el municipio de Pasca.</font></p>
+
+                        <h2>Descripción del recorrido </h2>
+
+                        <p><font size="3">El recorrido da inicio en el barrio Tiuhaque, de allí se realiza un desplazamiento hasta el barrio las violetas donde se realizara un calentamiento de aproximadamente 20 minutos, el guía de caminatas del IDRD dará las recomendaciones de la actividad y se continuara con la caminata.</font></p>
+
+                        <h2>Durante el recorrido se visitaran los siguientes atractivos:</h2>
+                        <ul class="ul_inicio">
+                            <font size="3">
+                            <li>Planta de tratamiento Yomasa</li>
+                            <li>Lago Yomasa</li>
+                            <li>Quebrada Yomasa</li>
+                            <li>Ecosistema de páramo</li>
+                            </font>
+                        </ul>
+                        <br>
+
+                        <font size="4"><center><table width="1000">
+
+                            <col width="281">
+
+                                <col width="282">
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Punto de encuentro</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Paradero de buses del barrio Tihuaque - Carrera 18 este con calle 86 sur antigua vía al llano</p>
+
+                                        </td>
+
+                                    </tr>
+                                     <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Barrio</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Tihuaque</p>
+
+                                        </td>
+
+                                    </tr>
+                              
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Fecha</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Domingo 11 de Diciembre de 2016</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Hora</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>8:00 a.m</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Cupo Máximo</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>40 Personsa</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Rango de Edad</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>12 - 60 Años</p>
+
+                                        </td>
+
+                                    </tr>
+
+
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Altitud</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>3.200 m.s.n.m</td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Temperatura</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>12°</p>
+
+                                        </td>
+
+                                    </tr>
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Nivel</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Alto</p>
+
+                                        </td>
+
+                                    </tr>
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Duración</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>3 - 4 horas aproximadamente</p>
+
+                                        </td>
+
+                                    </tr>
+                                     
+
+                        </table></center></font>
+
+                        <h2>Recomendaciones</h2>
+
+                        <ul class="ul_inicio">
+
+                            <font size="3">
+
+                                <li>Llevar ropa cómoda</li>
+                            
+                                <li>Llevar bloqueador solar</li>
+
+                                <li>Hidratación</li>
+
+                                <li>Calzado cómodo</li>
+
+                                <li>Llevar carné de la eps</li>
+
+                                <li>Refrigerio.</li>
+
+                                <li>Medicamentos personales </li>
+
+                                <li>No llevar mascotas </li>
+
+                                <li>Abstenerse de participar: mujeres en estado de embarazo, personas que tengan problemas de salud complejos</li>
+
+                                <li><ins>Abstenerse de asistir si no se encuentra inscrito</ins></li>
+                            </font>
+
+                            <br>
+
+                        </ul> <a class="btn btn-info" href="registro">Registrarse</a> </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+</section>
+
+@stop
